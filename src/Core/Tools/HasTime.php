a1689a1643470c881d0a92e5a0055a288f59f316
Benoît Mallo <benoit.mallo@bappli.com>
2017-09-01 17:01:27 +0200

    IMP Separate created_at and updated_at fields in the distinct trait

--- src/Core/Tools/HasTime.php
+++ src/Core/Tools/HasTime.php
@@ -4,45 +4,8 @@ namespace Core\Tools;
 
 trait HasTime
 {
-    /**
-     * @var \DateTime
-     */
-    public $createdAt;
 
-    /**
-     * @var \DateTime
-     */
-    public $updatedAt;
+    use HasCreatedAt;
+    use HasUpdatedAt;
 
-    /**
-     * @return \DateTime
-     */
-    public function getCreatedAt(): \DateTime
-    {
-        return $this->createdAt;
-    }
-
-    /**
-     * @param $created_at \DateTime
-     */
-    public function setCreatedAt($created_at)
-    {
-        $this->createdAt = $created_at;
-    }
-
-    /**
-     * @return \DateTime
-     */
-    public function getUpdatedAt(): \DateTime
-    {
-        return $this->updatedAt;
-    }
-
-    /**
-     * @param \DateTime $updatedAt
-     */
-    public function setUpdatedAt(\DateTime $updatedAt)
-    {
-        $this->updatedAt = $updatedAt;
-    }
 }
