8beed2bef5556723b667ec9d65a5f01476ab3270
Benoît Mallo <benoit.mallo@gmail.com>
2017-09-03 13:33:36 +0000

    ADD Category

--- res/routes.php
+++ res/routes.php
@@ -2,6 +2,7 @@
 
 use Core\Router\Router;
 use TuxBoy\Application\Blog\Controller\BlogController;
+use TuxBoy\Application\Blog\Controller\CategoryController;
 use TuxBoy\Application\Home\Controller\HomeController;
 
 $router = $app->container->get(Router::class);
@@ -11,3 +12,5 @@ $router->get('/blog', [BlogController::class, 'index'], 'blog.index');
 $router->get('/blog/new', [BlogController::class, 'create'], 'blog.new');
 $router->get('/blog/{slug}', [BlogController::class, 'show'], 'blog.show');
 $router->post('/blog/new', [BlogController::class, 'create']);
+$router->get('/blog/categorie/new', [CategoryController::class, 'create'], 'blog.category.new');
+$router->post('/blog/categorie/new', [CategoryController::class, 'create']);
