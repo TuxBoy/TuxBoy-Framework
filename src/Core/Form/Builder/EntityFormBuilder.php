ad3ae464553ae417590da3484a01957c2d474ba5
Benoît Mallo <benoit.mallo@bappli.com>
2017-09-21 17:11:14 +0200

    ADD Generate form to entity

---
+++ src/Core/Form/Builder/EntityFormBuilder.php
@@ -0,0 +1,76 @@
+<?php
+namespace TuxBoy\Form\Builder;
+
+use Doctrine\DBAL\Types\Type;
+use TuxBoy\Entity;
+use TuxBoy\Form\Button;
+use TuxBoy\Form\Input;
+use TuxBoy\Form\Textarea;
+use TuxBoy\ReflectionAnnotation;
+
+/**
+ * Class EntityFormBuilder
+ */
+class EntityFormBuilder
+{
+
+	/**
+	 * @var FormBuilder
+	 */
+	private $formBuilder;
+
+	/**
+	 * EntityFormBuilder constructor.
+	 *
+	 * @param FormBuilder $formBuilder
+	 */
+	public function __construct(FormBuilder $formBuilder)
+	{
+		$this->formBuilder = $formBuilder;
+	}
+
+	/**
+	 * Génère un formulaire à partir d'une entity.
+	 *
+	 * @param Entity $entity
+	 * @param string|null $action
+	 * @return string
+	 */
+	public function generateForm(Entity $entity, ?string $action = null): string
+	{
+		// En gros ça va lire toutes les proriétés de l'entity afin de construire le formulaire
+		if (is_null($action)) {
+			$action = '#';
+		}
+		$this->formBuilder->openForm($action, 'POST');
+		foreach (array_keys(get_object_vars($entity)) as $property) {
+			$propertyAnnotation = new ReflectionAnnotation($entity, $property);
+			if (
+				$propertyAnnotation->hasAnnotation('var')
+				&& $propertyAnnotation->getAnnotation('var')->getValue() === Type::STRING
+			) {
+				$value = null;
+				if ($entity->$property) {
+					$value = $entity->$property;
+				}
+				$this->formBuilder->add(
+					(new Input($property, $value))->setAttribute('type', Type::TEXT)
+				);
+			}
+
+			if (
+				$propertyAnnotation->hasAnnotation('var')
+				&& $propertyAnnotation->getAnnotation('var')->getValue() === Type::TEXT
+			) {
+				$value = null;
+				if ($entity->$property) {
+					$value = $entity->$property;
+				}
+				$this->formBuilder->add((new Textarea($property, $value)));
+			}
+		}
+		$this->formBuilder->add((new Button('Envoyer'))->setAttribute('type', 'submit'));
+		return $this->formBuilder->build();
+	}
+
+}
--- src/Core/Twig/FormExtension.php
+++ src/Core/Twig/FormExtension.php
@@ -2,17 +2,31 @@
 
 namespace TuxBoy\Twig;
 
+use TuxBoy\Entity;
+use TuxBoy\Form\Builder\EntityFormBuilder;
 use TuxBoy\Form\Builder\FormBuilder;
 
 class FormExtension extends \Twig_Extension
 {
-    /**
+
+	/**
+	 * @var EntityFormBuilder
+	 */
+	private $entityFormBuilder;
+
+	public function __construct(EntityFormBuilder $entityFormBuilder)
+	{
+		$this->entityFormBuilder = $entityFormBuilder;
+	}
+
+	/**
      * @return \Twig_SimpleFunction[]
      */
     public function getFunctions(): array
     {
         return [
-            new \Twig_SimpleFunction('form', [$this, 'getForm'], ['is_safe' => ['html']])
+            new \Twig_SimpleFunction('formBuilder', [$this, 'getForm'], ['is_safe' => ['html']]),
+            new \Twig_SimpleFunction('form', [$this, 'getFormWithEntity'], ['is_safe' => ['html']])
         ];
     }
 
@@ -25,4 +39,14 @@ class FormExtension extends \Twig_Extension
     {
         return (string) $formBuilder->build();
     }
+
+	/**
+	 * @param Entity      $entity
+	 * @param null|string $path
+	 * @return string
+	 */
+    public function getFormWithEntity(Entity $entity, ?string $path = null): string
+		{
+			return (string) $this->entityFormBuilder->generateForm($entity, $path);
+		}
 }
